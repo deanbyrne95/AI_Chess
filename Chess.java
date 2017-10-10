2a9d77d26cdf159d205d0c2140babd0183593e40
Dean Byrne <deanbyrne95@gmail.com>
2017-10-10 16:19:11 +0100

    Adding Java imports

--- Chess.java
+++ Chess.java
@@ -1,3 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.Stack;
+import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
 public class Chess extends JFrame implements MouseListener, MouseMotionListener
 {
 	public static void main(String[] args)
