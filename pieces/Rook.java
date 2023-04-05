c5729ca84e96737c72cf70b095ebc790c6036f83
Dean Byrne <deanbyrne95+github@gmail.com>
2023-04-05 17:07:53 +0100

    Update Rook; implement abstract methods

--- pieces/Rook.java
+++ pieces/Rook.java
@@ -10,6 +10,23 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean isValidMove(JPanel board, int newX, int newY) {
-        return super.canMove(board, newX, newY);
+        return this.canMove(board, newX, newY);
+    }
+
+    @Override
+    protected boolean canMove(JPanel board, int x, int y) {
+        if (super.outOfBounds(x, y) || !super.hasMoved(x, y)) return false;
+
+        return super.canMove(board, x, y);
+    }
+
+    @Override
+    protected boolean canMoveX(int from, int to, int numberSquares) {
+        return false;
+    }
+
+    @Override
+    protected boolean canMoveY(int from, int to, int numberSquares) {
+        return false;
     }
 }
