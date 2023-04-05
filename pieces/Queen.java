3f53f0bba59dd20635c6370869a7dd22aef915ce
Dean Byrne <deanbyrne95+github@gmail.com>
2023-04-05 17:07:49 +0100

    Update Queen; implement abstract methods

--- pieces/Queen.java
+++ pieces/Queen.java
@@ -12,4 +12,14 @@ public class Queen extends ChessPiece {
     public boolean isValidMove(JPanel board, int newX, int newY) {
         return super.canMove(board, newX, newY);
     }
+
+    @Override
+    protected boolean canMoveX(int from, int to, int numberSquares) {
+        return false;
+    }
+
+    @Override
+    protected boolean canMoveY(int from, int to, int numberSquares) {
+        return false;
+    }
 }
