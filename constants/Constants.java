e49d24644c424000c05f02224b5a21f2a5571553
Dean Byrne <deanbyrne95+github@gmail.com>
2023-04-05 16:06:33 +0100

    add Constants

---
+++ constants/Constants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Constants {
+    public static final Integer GRID_SIZE = 8;
+    public static final Integer SQUARE_SIZE = 75;
+    public static final Integer START_WHITE = 6;
+    public static final Integer START_BLACK = 1;
+}
