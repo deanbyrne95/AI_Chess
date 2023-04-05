736bd992484e75edeb0872ee73719700d354bbca
Dean Byrne <deanbyrne95+github@gmail.com>
2023-04-05 18:26:18 +0100

    Add Rook movement logic

--- pieces/Rook.java
+++ pieces/Rook.java
@@ -2,6 +2,8 @@ package pieces;
 
 import javax.swing.*;
 
+import static constants.Constants.SQUARE_SIZE;
+
 public class Rook extends ChessPiece {
 
     public Rook(int x, int y, boolean white) {
@@ -10,14 +12,17 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean isValidMove(JPanel board, int newX, int newY) {
+
         return this.canMove(board, newX, newY);
     }
 
     @Override
     protected boolean canMove(JPanel board, int x, int y) {
-        if (super.outOfBounds(x, y) || !super.hasMoved(x, y)) return false;
+        var landX = x / SQUARE_SIZE;
+        var landY = y / SQUARE_SIZE;
+        if (this.getInitialY() != landY && this.getInitialX() != landX) return false;
 
-        return super.canMove(board, x, y);
+        return super.canMove(board, x, y) && !super.isBlocked(board, landX, landY);
     }
 
     @Override
