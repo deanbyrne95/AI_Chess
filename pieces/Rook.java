fc2dee653a29b8da78f5890a6264d2850d0530b8
Dean Byrne <4136983+deanbyrne95@users.noreply.github.com>
2023-04-06 17:19:41 +0100

    Annoying rook (#1)
    * Revert "Add Rook movement logic"
    
    This reverts commit 736bd992484e75edeb0872ee73719700d354bbca.
    
    * Revert "Update logic for X/Y piece movement"
    
    This reverts commit 0686ea1aa6d418b9f68c19264bcce4c31bef2137.
    
    * rename method
    
    * remove log
    
    * fix Pawn movement

--- pieces/Rook.java
+++ pieces/Rook.java
@@ -2,8 +2,6 @@ package pieces;
 
 import javax.swing.*;
 
-import static constants.Constants.SQUARE_SIZE;
-
 public class Rook extends ChessPiece {
 
     public Rook(int x, int y, boolean white) {
@@ -12,17 +10,14 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean isValidMove(JPanel board, int newX, int newY) {
-
         return this.canMove(board, newX, newY);
     }
 
     @Override
     protected boolean canMove(JPanel board, int x, int y) {
-        var landX = x / SQUARE_SIZE;
-        var landY = y / SQUARE_SIZE;
-        if (this.getInitialY() != landY && this.getInitialX() != landX) return false;
+        if (super.outOfBounds(x, y) || super.notMoved(x, y)) return false;
 
-        return super.canMove(board, x, y) && !super.isBlocked(board, landX, landY);
+        return super.canMove(board, x, y);
     }
 
     @Override
